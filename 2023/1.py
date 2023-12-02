4b8ff7e9acc6a576af9c96751ad6804762498995
Maciej Borkowski <mmborkowski11@gmail.com>
2023-12-02 11:58:06 +0100

    day 1 2023, part 2

--- 2023/1.py
+++ 2023/1.py
@@ -1,4 +1,5 @@
 import re
+from word2number import w2n
 
 puzzle_input = []
 
@@ -21,3 +22,22 @@ for i in puzzle_input:
         digits.append(int((digit_regex[0]) + digit_regex[0]))
 
 print(f'The sum of all the callibrations is: {sum(digits)}')
+digits.clear()
+
+for i in puzzle_input:
+    digit_regex = re.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', i)
+
+    for j in range(len(digit_regex)):
+        if isinstance(digit_regex[j], str):
+            digit_regex[j] = w2n.word_to_num(digit_regex[j])
+
+    if len(digit_regex) == 2:
+        digits.append(int((digit_regex[0] * 10) + digit_regex[1]))
+
+    elif len(digit_regex) > 2:
+        digits.append(int((digit_regex[0] * 10) + digit_regex[-1]))
+
+    else:
+        digits.append(int((digit_regex[0] * 10) + digit_regex[0]))
+
+print(f'The sum of all the callibrations with words is: {sum(digits)}')
