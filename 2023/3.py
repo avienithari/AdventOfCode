e01eb5adb446d4a91f74a4a080eef5d56d6f4de4
Maciej Borkowski <mmborkowski11@gmail.com>
2024-01-01 21:26:25 +0100

    cleaning day 3

--- 2023/3.py
+++ 2023/3.py
@@ -2,6 +2,26 @@ with open('input3.txt', 'r') as puzzle:
     rows = puzzle.readlines()
     puzzle_input = [x.rstrip() for x in rows]
 
+def loop_over(i, j, coordinates):
+    for row in [i - 1, i, i + 1]:
+        for collumn in [j - 1, j, j + 1]:
+            if row < 0 or row >= len(puzzle_input) or collumn < 0 or collumn >= len(puzzle_input[row]) or not puzzle_input[row][collumn].isdigit():
+                continue
+            while collumn > 0 and puzzle_input[row][collumn - 1].isdigit():
+                collumn -= 1
+            coordinates.add((row, collumn))
+
+def calc(coordinates):
+    numbers = []
+
+    for row, collumn in coordinates:
+        number = ""
+        while collumn < len(puzzle_input[row]) and puzzle_input[row][collumn].isdigit():
+            number += puzzle_input[row][collumn]
+            collumn += 1
+        numbers.append(int(number))
+
+    return numbers
 
 def part_1():
     coordinates = set()
@@ -10,25 +30,10 @@ def part_1():
         for j, char in enumerate(row):
             if char.isdigit() or char == '.':
                 continue
-            for c_row in [i - 1, i, i + 1]:
-                for collumn in [j - 1, j, j + 1]:
-                    if c_row < 0 or c_row >= len(puzzle_input) or collumn < 0 or collumn >= len(puzzle_input[c_row]) or not puzzle_input[c_row][collumn].isdigit():
-                        continue
-                    while collumn > 0 and puzzle_input[c_row][collumn - 1].isdigit():
-                        collumn -= 1
-                    coordinates.add((c_row, collumn))
+            loop_over(i, j, coordinates)
 
-    numbers = []
-
-    for c_row, collumn in coordinates:
-        number = ""
-        while collumn < len(puzzle_input[c_row]) and puzzle_input[c_row][collumn].isdigit():
-            number += puzzle_input[c_row][collumn]
-            collumn += 1
-        numbers.append(int(number))
-
+    numbers = calc(coordinates)
     print(f'Part 1: {sum(numbers)}')
-
 
 def part_2():
     total = 0
@@ -40,29 +45,15 @@ def part_2():
 
             coordinates = set()
 
-            for c_row in [i - 1, i, i + 1]:
-                for collumn in [j - 1, j, j + 1]:
-                    if c_row < 0 or c_row >= len(puzzle_input) or collumn < 0 or collumn >= len(puzzle_input[c_row]) or not puzzle_input[c_row][collumn].isdigit():
-                        continue
-                    while collumn > 0 and puzzle_input[c_row][collumn - 1].isdigit():
-                        collumn -= 1
-                    coordinates.add((c_row, collumn))
-
+            loop_over(i, j, coordinates)
             if len(coordinates) != 2:
                 continue
 
-            numbers = []
-
-            for c_row, collumn in coordinates:
-                number = ""
-                while collumn < len(puzzle_input[c_row]) and puzzle_input[c_row][collumn].isdigit():
-                    number += puzzle_input[c_row][collumn]
-                    collumn += 1
-                numbers.append(int(number))
-
+            numbers = calc(coordinates)
             total += numbers[0] * numbers[1]
 
     print(f'Part 2: {total}')
+
 
 part_1()
 part_2()
