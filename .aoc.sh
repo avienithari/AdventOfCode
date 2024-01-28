b49efdd900135a443fa68ff538b4c33d4f463a05
Maciej Borkowski <mmborkowski11@gmail.com>
2024-01-28 11:48:50 +0100

    new python boiler

--- .aoc.sh
+++ .aoc.sh
@@ -1,7 +1,12 @@
 #!/bin/zsh
 
 
-echo "with open('input$1.txt', 'r') as puzzle:
-    rows = puzzle.readlines()
-    puzzle_input = [x for x in rows]
+echo "import sys
+
+puzzle_input = open(sys.argv[1], 'r').read().split()
+for line in puzzle_input:
+    print(line)
+
 " >> $1.py
+
+touch input$1.txt
