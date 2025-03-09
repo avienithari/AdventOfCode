388e06c36bb7e1e85ee89945986a85e13268302c
Maciej Borkowski <mmborkowski11@gmail.com>
2025-03-09 15:39:07 +0100

    feat(2021): day 12 part 2

--- 2021/day12.lua
+++ 2021/day12.lua
@@ -65,11 +65,12 @@ end
 ---@param input table
 ---@return integer
 local function part_1(input)
-  local adj = aoc.defaultdict()
   local visited = {}
+  local adj = aoc.defaultdict(function()
+    return {}
+  end)
 
   local caves = split_caves(input)
-
   for _, pair in ipairs(caves) do
     local a, b = pair[1], pair[2]
     table.insert(adj[a], b)
@@ -80,3 +81,63 @@ local function part_1(input)
 end
 
 aoc.print(1, part_1(example), part_1(puzzle))
+
+---@param cave string
+---@param adj table
+---@param visited table
+---@param visited_twice integer
+---@return integer
+---@diagnostic disable-next-line: redefined-local
+local function dfs(cave, adj, visited, visited_twice)
+  if cave == "end" then
+    return 1
+  end
+
+  if is_small(cave) then
+    visited[cave] = (visited[cave] or 0) + 1
+
+    if visited[cave] == 2 then
+      visited_twice = visited_twice + 1
+    end
+
+    if visited[cave] > 2 or visited_twice > 1 then
+      visited[cave] = visited[cave] - 1
+      return 0
+    end
+  end
+
+  local count = 0
+  for _, nbr in ipairs(adj[cave]) do
+    if nbr ~= "start" then
+      count = count + dfs(nbr, adj, visited, visited_twice)
+    end
+  end
+
+  if is_small(cave) then
+    visited[cave] = visited[cave] - 1
+  end
+
+  return count
+end
+
+---@param input table
+---@return integer
+local function part_2(input)
+  local visited = aoc.defaultdict(function()
+    return 0
+  end)
+  local adj = aoc.defaultdict(function()
+    return {}
+  end)
+
+  local caves = split_caves(input)
+  for _, pair in ipairs(caves) do
+    local a, b = pair[1], pair[2]
+    table.insert(adj[a], b)
+    table.insert(adj[b], a)
+  end
+
+  return dfs("start", adj, visited, 0)
+end
+
+aoc.print(2, part_2(example), part_2(puzzle))
