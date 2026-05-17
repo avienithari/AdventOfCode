8486831644acd9906779c1066ed366b331efeec1
avienithari <125766902+avienithari@users.noreply.github.com>
2026-05-17 23:59:38 +0200

    feat(2025): day 4 part 2

--- 2025/puzzles/day4.nix
+++ 2025/puzzles/day4.nix
@@ -15,6 +15,7 @@ let
     @.@.@@@.@.
   '';
   expectedPart1Test = 13;
+  expectedPart2Test = 43;
 
   aocInput = readFile inputPath;
 
@@ -29,11 +30,6 @@ let
       height = length grid;
       width = stringLength (head grid);
 
-      getCell = y: x:
-        if y >= 0 && y < height && x >= 0 && x < width
-        then substring x 1 (elemAt grid y)
-        else ".";
-
       directions = [
         { dy = -1; dx = -1; }
         { dy = -1; dx = 0; }
@@ -45,37 +41,87 @@ let
         { dy = 1; dx = 1; }
       ];
 
-      countAdj = y: x:
+      rollsInit = listToAttrs (
+        concatMap
+          (y:
+            let row = elemAt grid y;
+            in
+            concatMap
+              (x:
+                if substring x 1 row == "@"
+                then
+                  [{
+                    name = "${toString y}, ${toString x}";
+                    value = { inherit y x; };
+                  }]
+                else [ ]
+              )
+              (genList (i: i) width)
+          )
+          (genList (i: i) height)
+      );
+
+      countNeighbors = state: y: x:
         foldl'
           (acc: dir:
-            if getCell (y + dir.dy) (x + dir.dx) == "@" then
-              acc + 1 else acc
+            if state ? "${toString (y + dir.dy)}, ${toString (x + dir.dx)}"
+            then acc + 1 else acc
           ) 0
           directions;
 
-      processRow = totalAcc: y:
+      getRemoveableNames = state:
         let
-          processCol = rowAcc: x:
-            if getCell y x == "@" && countAdj y x < 4
-            then rowAcc + 1
-            else rowAcc;
+          removeableRolls = filter
+            (
+              roll: countNeighbors state roll.y roll.x < 4
+            )
+            (attrValues state);
         in
-        totalAcc + foldl' processCol 0 (genList (i: i) width);
+        map (roll: "${toString roll.y}, ${toString roll.x}") removeableRolls;
+
+      p1Removeable = getRemoveableNames rollsInit;
+      part1Sum = length p1Removeable;
+
+      cascade = state: removedCount:
+        let
+          toRemove = getRemoveableNames state;
+          numToRemove = length toRemove;
+        in
+        if numToRemove == 0
+        then removedCount
+        else
+          cascade (removeAttrs state toRemove) (removedCount + numToRemove);
+
+      part2Sum = cascade rollsInit 0;
     in
-    foldl' processRow 0 (genList (i: i) height);
+    {
+      inherit part1Sum part2Sum;
+    };
 
   test = solve testInput;
   solution = solve aocInput;
 in
 {
   part1 =
-    if test != expectedPart1Test
+    if test.part1Sum != expectedPart1Test
     then
       throw ''
         [TEST FAILED]
-        Got:      ${toString test}
+        Got:      ${toString test.part1Sum}
         Expected: ${toString expectedPart1Test}
       ''
     else
-      solution;
+      solution.part1Sum;
+
+  part2 =
+    if
+      test.part2Sum != expectedPart2Test
+    then
+      throw ''
+        [TEST FAILED]
+        Got:      ${toString test.part2Sum}
+        Expected: ${toString expectedPart2Test}
+      ''
+    else
+      solution.part2Sum;
 }
