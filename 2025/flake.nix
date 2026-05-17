5bbf5f93e5bb766e28de7e1a4c43ab031bf4c6b8
avienithari <125766902+avienithari@users.noreply.github.com>
2026-05-17 16:40:09 +0200

    feat(2025): adding nix flake and .envrc

---
+++ 2025/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "AOCix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nixd
+            nixfmt
+            deadnix
+            statix
+          ];
+        };
+      }
+    );
+}
