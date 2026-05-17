17a412a9eb74b748c1a62020e62810c0b44afa80
avienithari <125766902+avienithari@users.noreply.github.com>
2026-05-17 17:46:56 +0200

    feat(2025): getting puzzle input

--- 2025/flake.nix
+++ 2025/flake.nix
@@ -10,6 +10,45 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+
+        aoc-nix = pkgs.writeShellScriptBin "aoc-nix" ''
+          if [ -f .env ]; then
+            source .env
+          else
+            echo "Error: no .env with aoc session cookie found."
+            exit 1
+          fi
+
+          if [ -z "$1" ]; then
+            DAYS=$(find ./puzzles -maxdepth 1 -name "day*.nix" | \
+              sed -n 's/.*day\([0-9]\+\)\.nix/\1/p' | sort -n)
+
+            if [ -z "$DAYS" ]; then
+              echo "No puzzles found in ./puzzles."
+              exit 0
+            fi
+          else
+            DAYS="$1"
+          fi
+
+          mkdir -p ./input/
+
+          for DAY in $DAYS; do
+            INPUT_FILE="./input/day''${DAY}.txt"
+
+            if [ ! -f "$INPUT_FILE" ]; then
+              curl -s --cookie "session=$AOC_SESSION" \
+                "https://adventofcode.com/2025/day/$DAY/input" > "$INPUT_FILE"
+            fi
+
+            git add -f "$INPUT_FILE"
+
+            echo "--- Evaluating Day: $DAY ---"
+            nix eval --file ./main.nix --apply "f: f \"$DAY\""
+
+            git rm --cached --force "$INPUT_FILE" >/dev/null 2>&1
+          done
+        '';
       in
       {
         devShells.default = pkgs.mkShell {
@@ -18,6 +57,8 @@
             nixfmt
             deadnix
             statix
+
+            aoc-nix
           ];
         };
       }
